5e579b00cdab4c4f8d0bc86ee389bd7aca48eb8a
LatoriMiller <latorimiller@Latoris-MacBook-Pro-2.local>
2017-01-11 22:21:04 -0500

    first commit

---
+++ app/assets/javascripts/grayscale.js
@@ -0,0 +1,11 @@
+//JQuery to collapse th navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
